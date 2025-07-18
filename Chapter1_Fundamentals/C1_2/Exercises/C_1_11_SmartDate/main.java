22c358488d7ade2c6e7d8f8af9dc527205233345
dopesthetic <150394396+tyudope@users.noreply.github.com>
2025-07-18 16:35:09 +0200

    Implement Exercise 1.2.11: SmartDate with test cases - Added validation for month range (1-12) - Implemented day validation based on the month and leap year - Created helper methods; daysInMonth() and isLeapYear() - Threw IllegalArgumentException when input is invalid - Tested with valid and invalid dates.

---
+++ Chapter1_Fundamentals/C1_2/Exercises/C_1_11_SmartDate/main.java
@@ -0,0 +1,19 @@
+package Chapter1_Fundamentals.C1_2.Exercises.C_1_11_SmartDate;
+
+public class main {
+    public static void main(String[] args) {
+
+        try {
+            SmartDate valid = new SmartDate(2024, 2, 29);
+        } catch (IllegalArgumentException e) {
+            System.out.println(" Failed: 2024-02-29 → " + e.getMessage());
+        }
+
+        try {
+            SmartDate invalidMonth = new SmartDate(2023, 13, 1);
+        } catch (IllegalArgumentException e) {
+            System.out.println(" Failed: 2023-13-01 → " + e.getMessage());
+        }
+    }
+}
+
